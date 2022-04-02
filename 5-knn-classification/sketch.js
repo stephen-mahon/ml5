bed245223e20bc4bfb39ab86ac246924caa3e871
Stephen Mahon <stemahon@gmail.com>
2022-04-01 23:24:01 -0500

    knn application for video inferring clissification

--- 5-knn-classification/sketch.js
+++ 5-knn-classification/sketch.js
@@ -3,9 +3,13 @@
 // 2. Create a "feature extractor"
 // 3. Pass in image and get the logits values
 // 4. Create an empty ml5.KNN model
+// 5. "infer" from image (cosine distance)
+
 let video;
 let features;
 let knn;
+let lebelP;
+let ready = false;
 
 function setup() {
   createCanvas(320, 240);
@@ -14,11 +18,39 @@ function setup() {
   video.hide();
   features = ml5.featureExtractor("MobileNet", modelReady());
   knn = ml5.KNNClassifier();
+  labelP = createP("Need training data");
+  labelP.style('font-size', '32pt')
+}
+
+function goClassify() {
+  const logits = features.infer(video);
+  knn.classify(logits, function (error, result) {
+    if(error) {
+      console.log(error);
+    } else {
+      labelP.html(result.label);
+      goClassify();
+    }  
+  });
 }
 
 function mousePressed() {
+  if (knn.getNumLabels() > 0) {
+    const logits = features.infer(video);
+    knn.classify(logits, gotResult);
+  }
+}
+
+function keyPressed() {
   const logits = features.infer(video);
-  console.log(logits.dataSync());
+  if (key === 'l') {
+    knn.addExample(logits, "left");
+    console.log("left");
+  } else if (key === "r") {
+    knn.addExample(logits, "right");
+    console.log("right");
+  }
+  
 }
 
 function modelReady() {
@@ -27,4 +59,10 @@ function modelReady() {
 
 function draw() {
   image(video, 0, 0);
+
+  if (!ready && knn.getNumLabels() > 0) {
+    goClassify();
+    ready = true;
+  }
+
 }
