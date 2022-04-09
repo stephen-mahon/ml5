7a57b2f55b0b3dd3b3855f9723a5fe1bf469e06e
Stephen Mahon <stemahon@gmail.com>
2022-04-08 23:32:37 -0500

    posenet

--- 7-Pose-Estimation/sketch.js
+++ 7-Pose-Estimation/sketch.js
@@ -1,7 +1,43 @@
+let video;
+let poseNet;
+let pose;
+let skeleton;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 400);
+  video = createCapture(VIDEO)
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(poses) {
+  if (poses.length > 0) {
+    pose = poses[0].pose;
+    skeleton = poses[0].skeleton;
+  }
+}
+
+function modelLoaded() {
+  console.log("poseNet ready");
 }
 
 function draw() {
-  background(220);
+  image(video, 0, 0);
+  if (pose) {
+    fill(0, 255, 0);
+    for (let i = 0; i < pose.keypoints.length; i++) {
+      let x = pose.keypoints[i].position.x;
+      let y = pose.keypoints[i].position.y;
+      ellipse(x, y, 16);
+    }
+    for (let i = 0; i < skeleton.length; i++) {
+      let a = skeleton[i][0];
+      let b = skeleton[i][1];
+      strokeWeight(2);
+      stroke(255);
+      line(a.position.x, a.position.y, b.position.x, b.position.y )
+    }
+
+  }
 }
